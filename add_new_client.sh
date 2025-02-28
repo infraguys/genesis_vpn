a15b2fdee9805229501d85411eed168b98c57d36
George Melikov <mail@gmelikov.ru>
2025-02-28 18:46:36 +0300

    add_new_client.sh: Add license + minor validation fix

--- add_new_client.sh
+++ add_new_client.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 # Function to display help message
 show_help() {
@@ -6,8 +22,8 @@ show_help() {
     echo "Generate client config."
 }
 
-# Check if the first argument is set
-if [ -z "$1" ]; then
+# Check if the second argument is set
+if [ -z "$2" ]; then
     show_help
     exit
 fi
